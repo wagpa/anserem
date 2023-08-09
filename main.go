ee977b7d11fadbf6154931536c8ef749f27d70c5
Paul Johannes Wagner <hydrofin@gmail.com>
2023-08-09 21:25:30 +0200

    remove debug code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/urfave/cli/v3"
 	"io"
 	"log"
-	"net"
 	"net/http"
 	"net/mail"
 	"os"
@@ -100,13 +99,6 @@ func (o *Options) start(ctx *cli.Context) error {
 }
 
 func (o *Options) refresh() {
-
-	// TODO remove after debugging
-	if addrs, err := net.InterfaceAddrs(); err == nil {
-		for _, addr := range addrs {
-			log.Printf("found address %v", addr)
-		}
-	}
 
 	// send update request
 	res, err := http.Get(fmt.Sprintf(
